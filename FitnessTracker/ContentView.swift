092d1c3220d0bcab5a7b6a87c3eac285877fa01f
Victor Elujinmi <victorelujinmi@gmail.com>
2020-04-22 18:19:28 -0400

    Initial Commit

---
+++ FitnessTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessTracker
+//
+//  Created by Victor Elujinmi on 4/22/20.
+//  Copyright © 2020 V&M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
